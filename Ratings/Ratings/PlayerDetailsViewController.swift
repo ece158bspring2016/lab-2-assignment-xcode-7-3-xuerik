37208835e48f0d4b3afca0cf9271ca6b4381997f
Erik Xu <dixu@ucsd.edu>
2016-05-08 18:38:38 -0700

    - Completed "Introducting Segues" and "Static Cells"

---
+++ Ratings/Ratings/PlayerDetailsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PlayerDetailsViewController.swift
+//  Ratings
+//
+//  Created by Erik Xu on 5/8/16.
+//  Copyright © 2016 Erik Xu. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailsViewController: UITableViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var detailLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+    
+    
+    /*
+     * Allows user to tap anywhere on the keyboard to bring up keyboard
+     */
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 {
+            nameTextField.becomeFirstResponder()
+        }
+    }
+}
---
+++ Ratings/Ratings/SampleData.swift
@@ -0,0 +1,16 @@
+//
+//  SampleData.swift
+//  Ratings
+//
+//  Created by Erik Xu on 4/18/16.
+//  Copyright © 2016 Erik Xu. All rights reserved.
+//
+
+import Foundation
+
+
+//Set up sample data
+let playersData = [
+    Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+    Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
